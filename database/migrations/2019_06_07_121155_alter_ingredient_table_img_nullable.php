6987dc66392282eada25dbd07359342ea0a731e3
Kobo-one <kobe.christiaensen@hotmail.com>
2019-06-07 14:15:35 +0200

    Adding ingredients without image

--- database/migrations/2019_06_07_121155_alter_ingredient_table_img_nullable.php
+++ database/migrations/2019_06_07_121155_alter_ingredient_table_img_nullable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterIngredientTableAllImg extends Migration
+class AlterIngredientTableImgNullable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AlterIngredientTableAllImg extends Migration
     public function up()
     {
         Schema::table('ingredients', function (Blueprint $table) {
-            $table->string('img');
+            $table->string('img')->nullable()->change();;
         });
     }
 
