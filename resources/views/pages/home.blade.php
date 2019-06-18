e23c0f364dd71ef2c93d0f1b462999b95ad55098
Kobo-one <kobe.christiaensen@hotmail.com>
2019-06-18 10:55:11 +0200

    demo prepare

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
            <img class="logo" src="{{asset('img/logo-named.svg')}}" alt="Planeat Logo">
            <div class="image--full-width">
                <a href="{{route('login')}}" class="btn btn--primary mb-xsm">Log in</a><br>
-               <a href="{{route('register')}}" class="btn btn--secondary">Register</a>
+{{--               <a href="{{route('register')}}" class="btn btn--secondary">Register</a>--}}
            </div>
     </div>
 
